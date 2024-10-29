c1f8ca4a76d7bb6ede2db6fa5d0f0184808594d4
dangvannghia204 <dangvannghiadn@gmail.com>
2024-10-29 16:21:11 +0700

    Update Buoi4.java

--- C. HƯỚNG DẪN/BUỔI 4/Buoi4.java
+++ C. HƯỚNG DẪN/BUỔI 4/Buoi4.java
@@ -16,6 +16,32 @@ public class Buoi4 {
             System.out.printf("\nnguHanh[%d] = %s", i, nguHanh[i]);
         }
     }
+    public static void bai4_3()
+    {
+        int arr1[][] = new int[2][3];
+        System.out.println("\nDanh sách phần tử của mảng arr1 là: ");
+        for(int i=0; i<arr1.length; i++)
+        {
+            for(int j=0; j<arr1[i].length; j++)
+            {
+                System.out.printf("arr1[%d][%d]",i,j);
+            }
+            System.out.println();
+        }
+    }
+    public static void bai4_4()
+    {
+        int arr2[][] = {{1,2,3},{4,5,6},{7}};
+        System.out.println("\nDanh sách phần tử của mảng arr2 là: ");
+        for(int i=0; i<arr2.length; i++)
+        {
+            for(int j=0; j<arr2[i].length; j++)
+            {
+                System.out.printf("arr2[%d][%d] = %d\t",i,j, arr2[i][j]);
+            }
+            System.out.println();
+        }
+    }
     public static void bai4_2NC(int n)
     {
         System.out.printf("\nDanh sách các phần tử từ 1 đến n = %d là: ", n);
@@ -43,6 +69,8 @@ public class Buoi4 {
         num = sc.nextInt();
         bai4_1();
         bai4_2();
+        bai4_3();
+        bai4_4();
         bai4_2NC(num);
         bai4_3NC(num);
     }
